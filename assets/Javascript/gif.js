952ed88cc8b5d5444fac629f9863236a65f1ebce
bhelms2132 <bhelms2132@gmail.com>
2019-08-22 19:57:33 -0400

    array created

---
+++ assets/Javascript/gif.js
@@ -0,0 +1,14 @@
+//--Array of strings--//
+var topics = ["Carolina Panthers", "Jocko Wilink", "Jordan B. Peterson", "jquery", "javascript"];
+  console.log(topics);
+
+
+//--Funtion for displaying topics data--//
+function renderButtons() {
+   $("#buttons-view").empty();
+//--Looping through topics array--//   
+   for (var i = 0; i < topics.length; i++) {
+//--Adds button in jquery--//
+   var a = $("<button>");
+   }
+}
