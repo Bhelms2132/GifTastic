072d7f30af498e03227168f16f5ac3b3c72d9362
bhelms2132 <bhelms2132@gmail.com>
2019-08-24 18:32:48 -0400

    gif images appearing and some css added to the buttons and background

--- assets/Javascript/gif.js
+++ assets/Javascript/gif.js
@@ -1,30 +1,41 @@
 //--Array of strings--//
-var topics = ["Carolina Panthers", "Beach", "Zakk Wylde", "Tyler Childers", "Golf", "Javascript"];
+var topics = ["Carolina Panthers", "Beach", "Zakk Wylde", "Tyler Childers", "Golf", "DeadPool"];
   console.log(topics);
-  
 //--Funtion for displaying topics data--//
 function renderButtons() {
-   $("#buttons-view").empty();
+  $("#buttons-view").empty();
    //--Looping through topics array--//   
-     for (var i = 0; i < topics.length; i++) {
-     //--Adds button in jquery--//
-       var a = $("<button>");
-         a.addClass("topic");
-           a.attr("data-name", topics[i]);
-             a.text(topics[i]);
-       //--Adds button to the html--//
-         $("#buttons-view").append(a);
-   }
+   for (var i = 0; i < topics.length; i++) {
+    //--Adds button in jquery--//
+    var a = $("<button>");
+        a.addClass("topic");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+    //--Adds button to the html--//
+    $("#buttons-view").append(a);
+  }
 }
-
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=yPggTPiRiu1TWfJN8xHwQP5AtSRuOkcl";
-
-//--Ajax request with queryURL--//
-$.ajax({
-   url: queryURL,
-   method: "Get"
-})
-
+//--Click event listener added to buttons--//
+$(document).on("click", ".topic", function(){
+   console.log("button clicked");
+   var topic = $(this).attr("data-name")
+   var queryURL = "http:api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=yPggTPiRiu1TWfJN8xHwQP5AtSRuOkcl&limit=10";
+   //--Ajax request with queryURL--//
+    $.ajax({
+     url: queryURL,
+     method: "GET"
+    })
+    .then(function(response){
+      console.log(response);
+     console.log(response.data [0].rating);
+   //--Looping through each result item--//
+     for (var i = 0; i < response.data.length; i++){
+       console.log(response.data[i].rating);
+       $("#gif").prepend("<p>Rating: " + response.data[i].rating + "</p>");
+        $("#gif").prepend("<img src='" + response.data[i].images.downsized_still.url + "'>");
+     }
+    });
+});
 //--Form function--//
 $("#add-topic").on("click", function(event) {
    event.preventDefault();
