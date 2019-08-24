3815dec23636e0e3ed97f7359426149f1c857d2f
bhelms2132 <bhelms2132@gmail.com>
2019-08-23 20:46:26 -0400

    buttons removed from html and added back via jquery

--- assets/Javascript/gif.js
+++ assets/Javascript/gif.js
@@ -1,16 +1,32 @@
 //--Array of strings--//
-var topics = ["Carolina Panthers", "Jocko Wilink", "Beach", "Zakk Wylde", "Tyler Childers", "Golf", "Javascript"];
+var topics = ["Carolina Panthers", "Beach", "Zakk Wylde", "Tyler Childers", "Golf", "Javascript"];
   console.log(topics);
 
 
+//--Funtion for displaying topics data--//
+function renderButtons() {
+   $("#buttons-view").empty();
+   //--Looping through topics array--//   
+     for (var i = 0; i < topics.length; i++) {
+     //--Adds button in jquery--//
+       var a = $("<button>");
+         a.addClass("topic");
+           a.attr("data-name", topics[i]);
+             a.text(topics[i]);
+       //--Adds button to the html--//
+         $("#buttons-view").append(a);
+   }
+}
 
 
+
+//--Form function--//
 $("#add-topic").on("click", function(event) {
    event.preventDefault();
 //--Grabs text from the input box--//
    var topic = $("#topic-input").val().trim();
        topics.push(topic);
        renderButtons();
-       
 })
+renderButtons();
 
